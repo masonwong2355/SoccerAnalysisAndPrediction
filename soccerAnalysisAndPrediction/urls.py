5b772a02656bef47d84c6067b93c3b0b81b18d9b
Jc_revenger <84384337+masonwong2355@users.noreply.github.com>
2021-05-25 14:37:01 +0800

    upload image

--- soccerAnalysisAndPrediction/urls.py
+++ soccerAnalysisAndPrediction/urls.py
@@ -5,14 +5,15 @@ from . import views
 # app_name = 'soccerAnalysisAndPrediction'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('eva/', views.eva, name='eva'),
     path('prediction/', views.prediction, name='prediction'),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     path('predictResult/', views.predictResult, name='predictResult'),
     path('heatmap/', views.pol_MatchHeatmap, name='pol_MatchHeatmap'),
     path('passEvent/', views.plot_pass_events, name='plot_pass_events'),
-    path('overviewStatistics/', views.overviewStatistics, name='overviewStatistics'),
+    # path('overviewStatistics/', views.overviewStatistics, name='overviewStatistics'),
+    path('', views.overviewStatistics, name='index'),
     path('matchsCompetitions/<int:competition_id>/<int:season_id>', views.matchsCompetitions, name='matchsCompetitions'),
     path('matchAnalysis/<int:competition_id>/<int:season_id>/<int:match_id>', views.matchAnalysis, name='matchAnalysis'),
     # path('matchAnalysis/', views.matchAnalysis, name='matchAnalysis'),
